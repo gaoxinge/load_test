24c3dbdfb66be40660b061818d6c236c23cc0a14
gaoxinge <gaoxx5@gmail.com>
2023-05-09 16:24:08 +0800

    add setup; update readme

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 - get the latency of the module
 - get the latency and through output of the service
+
+## install
+
+```
+pip install git+https://github.com/gaoxinge/load_test
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="load_test",
+    author="gaoxinge",
+    version="0.0.1",
+    description="load_test",
+    url="https://github.com/gaoxinge/load_test",
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={},
+    include_package_data=True,
+)
