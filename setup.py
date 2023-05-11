694807b3cb8aa6c2d414133159940c4c83cfa641
gaoxinge <gaoxx5@gmail.com>
2023-05-11 23:12:06 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="load_test",
     author="gaoxinge",
-    version="0.0.1",
+    version="0.0.2",
     description="load_test",
     url="https://github.com/gaoxinge/load_test",
     packages=find_packages(),
